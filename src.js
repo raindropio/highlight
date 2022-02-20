64fceddb5e8e07d4c2340e28ea91c159465621ac
Rustem Mussabekov <winlinx@ya.ru>
2022-02-20 11:22:09 +0300

    Reusable tooltip component

--- src.js
+++ src.js
@@ -47,24 +47,227 @@ function RdPrompt(x, y, placeholder, callback){
     p.addEventListener('blur', p.close)
 }
 
-class RdSelect {
+class RdTooltip {
     _parent = null //RdHighlight
     _menu = null
+    _listeners = {} //{ onColorClick(color), onNoteClick(x,y) }
 
     _colors = ['yellow', 'blue', 'green', 'red']
 
-    _idMenu = 'rdhm'
-    _classButtonHighligh = 'rdhbh'
-    _idButtonNote = 'rdhbn'
+    _classMenu = 'rdhm'
+    _classButtonColor = 'rdhbh'
+    _classButtonNote = 'rdhbn'
     _idCss = 'rdhss'
     _attrColor = 'data-rdhsc'
 
-    constructor(parent) {
+    constructor(parent, { onColorClick, onNoteClick }) {
         this._parent = parent
+        this._listeners = { onColorClick, onNoteClick }
 
         //init
         this._initStyles()
         this._initMenu()
+
+        //bind
+        this.show = this.show.bind(this)
+        this.hide = this.hide.bind(this)
+    }
+
+    show(x, y) {
+        let left = x
+        let top = y
+        if (this._parent._window.outerWidth <= left + 64) left = left - 64
+        if (this._parent._window.scrollY > top) top = this._parent._window.scrollY
+        this._menu.setAttribute('style', `left: ${left}px !important; top: ${top}px !important;`)
+        this._menu.removeAttribute('hidden')
+    }
+
+    hide() {
+        if (this._menu) this._menu.setAttribute('hidden', 'true')
+    }
+
+    /* Buttons */
+    _colorClick(e) {
+        e.preventDefault()
+        if (typeof this._listeners.onColorClick != 'function') return
+        this._listeners.onColorClick(e.currentTarget.getAttribute(this._attrColor) || '')
+    }
+
+    _noteClick(e) {
+        e.preventDefault()
+        if (typeof this._listeners.onNoteClick != 'function') return
+        this._listeners.onNoteClick(e.screenX - 14, e.screenY - 14)
+    }
+
+    /* Menu */
+    _initMenu() {
+        if (this._menu)
+            return
+
+        //create menu
+        this._menu = this._parent._document.createElement('menu')
+        this._menu.className = this._classMenu
+        this._menu.setAttribute('hidden', 'true')
+        this._menu.innerHTML = `
+            <li title="Highlight">
+                ${this._colors.map(color=>`
+                    <button class="${this._classButtonColor}" ${this._attrColor}="${color}"></button>
+                `).join('')}
+            </li>
+            
+            <button class="${this._classButtonNote}" title="Add note...">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                    <g fill-rule="evenodd">
+                        <path fill-rule="nonzero" d="M15 1a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3v1.9a1 1 0 0 1-1.6.7L10.2 16H4a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3h11Zm0 1H4c-1 0-2 .8-2 1.9V13c0 1 .8 2 1.9 2h6.7l3.4 2.9V15h1c1 0 2-.8 2-1.9V4c0-1-.8-2-1.9-2H15Z"/>
+                        <path d="M10 5v3h3v1h-3v3H9V9H6V8h3V5h1Z"/>
+                    </g>
+                </svg>
+            </button>
+        `
+        this._parent._container.appendChild(this._menu)
+
+        //add click events
+        this._menu.querySelectorAll(`.${this._classButtonColor}`)
+            .forEach(e=>{
+                this._colorClick = this._colorClick.bind(this)
+                e.removeEventListener('click', this._colorClick)
+                e.addEventListener('click', this._colorClick)
+            })
+
+        this._menu.querySelectorAll(`.${this._classButtonNote}`)
+            .forEach(e=>{
+                this._noteClick = this._noteClick.bind(this)
+                e.removeEventListener('click', this._noteClick)
+                e.addEventListener('click', this._noteClick)
+            })
+    }
+
+    /* Styles */
+    _initStyles() {
+        if (this._parent._container.querySelector(`#${this._idCss}`))
+            return
+
+        const style = this._parent._document.createElement('style')
+        style.id = this._idCss
+        style.innerHTML = `
+            .${this._classMenu} {
+                position: absolute !important;
+                display: flex !important;
+                
+                z-index: 99999999 !important;
+                background-color: transparent !important;
+                margin: 4px !important;
+                width: auto !important;
+                height: auto !important;
+                left: 0 !important; top: 0 !important;
+                box-shadow: none !important;
+                animation: none !important;
+                transition: opacity .15s ease-in-out !important;
+                border: 0 !important;
+                padding: 0 !important;
+                backdrop-filter: blur(20px) !important;
+                -webkit-backdrop-filter: blur(20px) !important;
+            }
+            .${this._classMenu}, .${this._classMenu} *, .${this._classMenu}:after {
+                border-radius: 5px !important;
+            }
+            .${this._classMenu}:after {
+                content: '' !important;
+                position: absolute !important;
+                left: 0 !important; top: 0 !important; right: 0 !important; bottom: 0 !important;
+                z-index: -1 !important;
+                background-color: Menu !important;
+                box-shadow: 0 0 0 0.5px GrayText, 0 5px 30px rgb(0 0 0 / 30%) !important;
+            }
+            @supports (backdrop-filter: blur(20px)) {
+                .${this._classMenu}:after { opacity: .6 !important; }
+            }
+            @supports (-webkit-backdrop-filter: blur(20px)) {
+                .${this._classMenu}:after {
+                    opacity: .6 !important;
+                    background-color: Window !important;
+                }
+            }
+            .${this._classMenu}, .${this._classMenu} * {
+                box-sizing: border-box !important;
+                user-select: none !important;
+                -webkit-user-select: none !important;
+            }
+            .${this._classMenu}[hidden] {
+                pointer-events: none !important;
+                opacity: 0 !important;
+            }
+
+            /* Dropdown */
+            .${this._classMenu} > li {
+                display: block !important;
+                max-height: 32px !important;
+                transition: max-height .15s ease-in !important;
+                transition-delay: .25s !important;
+                overflow: hidden !important;
+            }
+
+            .${this._classMenu} > li:hover:not(:active) {
+                max-height: ${this._colors.length * 32}px !important;
+            }
+
+            /* Buttons */
+            .${this._classMenu} button {
+                cursor: pointer !important;
+                color: WindowText !important;
+                width: 32px !important;
+                height: 32px !important;
+                appearance: none !important;
+                background: transparent !important;
+                border: 0 !important;
+                box-shadow: none !important;
+                margin: 0 !important;
+                padding: 0 !important;
+                display: flex !important;
+                align-items: center !important;
+                justify-content: center !important;
+                transition: background .1s linear, color .1s linear !important;
+                filter: none !important;
+            }
+            .${this._classMenu} button:hover {
+                background: rgba(100,100,100,.3) !important;
+            }
+            .${this._classMenu} button:active {
+                filter: brightness(50%) !important;
+            }
+
+            .${this._classMenu} * {
+                fill: currentColor !important;
+            }
+
+            /* Color */
+            .${this._classMenu} button[${this._attrColor}]:before {
+                content: '' !important;
+                display: block !important;
+                width: 18px !important;
+                height: 18px !important;
+                border-radius: 18px !important;
+                box-shadow: inset 0 0 0 0.5px rgba(0,0,0,.4) !important;
+                background-image: linear-gradient(to bottom, rgba(255,255,255,.5) 0, rgba(255,255,255,.4) 100%) !important;
+            }
+            ${this._colors.map(color=>`
+                .${this._classMenu} button[${this._attrColor}=${color}]:before { background-color: ${color} !important; }
+            `).join('')}
+        `
+        this._parent._container.appendChild(style)
+    }
+}
+
+class RdSelection {
+    _parent = null //RdHighlight
+    _tooltip = null
+
+    constructor(parent) {
+        this._parent = parent
+        this._tooltip = new RdTooltip(parent, {
+            onColorClick: color=>this._parent.addSelection({ color }),
+            onNoteClick: (x, y)=>this._parent.noteSelection(x, y)
+        })
 
         //bind
         this.render = this.render.bind(this)
@@ -83,209 +286,31 @@ class RdSelect {
     }
 
     render() {
-        const selection = this._parent._window.getSelection()
-
-        //remove if no selection yet
+        //remove tooltip if no selection yet
         if (!this._parent.enabled || 
             !this._haveSelection() || 
             !this._parent._document.hasFocus()){
-            if (this._menu) this._menu.setAttribute('hidden', 'true')
+            this._tooltip.hide()
             return
         }
 
         //position menu
+        const selection = this._parent._window.getSelection()
         const { x, y, width } = selection.getRangeAt(0).getBoundingClientRect()
         let left = this._parent._window.scrollX+x+width
         let top = this._parent._window.scrollY+y-32
-        if (this._parent._window.outerWidth <= left + 64) left = left - 64
-        if (this._parent._window.scrollY > top) top = this._parent._window.scrollY
-        this._menu.setAttribute('style', `left: ${left}px !important; top: ${top}px !important;`)
-        this._menu.removeAttribute('hidden')
+        this._tooltip.show(left, top)
     }
 
+    /* User changed document selection event */
     _haveSelection() {
         const selection = this._parent._window.getSelection()
         return selection.rangeCount && !selection.isCollapsed && selection.toString().trim()
     }
 
-    /* User changed document selection event */
     _onSelectionChange() {
         clearTimeout(this._selectTimeout)
-        this._selectTimeout = setTimeout(
-            this.render,
-            this._haveSelection() ? 200 : 0
-        )
-    }
-
-    /* Buttons */
-    _addClick(e) {
-        e.preventDefault()
-        this._parent.addSelection({
-            color: e.currentTarget.getAttribute(this._attrColor) || ''
-        })
-    }
-
-    _addNoteClick(e) {
-        e.preventDefault()
-        this._parent.noteSelection(e.screenX - 14, e.screenY - 14)
-    }
-
-    /* Menu */
-    _initMenu() {
-        if (this._menu)
-            return
-
-        //create menu
-        this._menu = this._parent._document.createElement('menu')
-        this._menu.id = this._idMenu
-        this._menu.setAttribute('hidden', 'true')
-        this._menu.innerHTML = `
-            <li title="Add highlight...">
-                ${this._colors.map(color=>`
-                    <button class="${this._classButtonHighligh}" ${this._attrColor}="${color}"></button>
-                `).join('')}
-            </li>
-            
-            <button id="${this._idButtonNote}" title="Add note...">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-                    <g fill-rule="evenodd">
-                        <path fill-rule="nonzero" d="M15 1a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3v1.9a1 1 0 0 1-1.6.7L10.2 16H4a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3h11Zm0 1H4c-1 0-2 .8-2 1.9V13c0 1 .8 2 1.9 2h6.7l3.4 2.9V15h1c1 0 2-.8 2-1.9V4c0-1-.8-2-1.9-2H15Z"/>
-                        <path d="M10 5v3h3v1h-3v3H9V9H6V8h3V5h1Z"/>
-                    </g>
-                </svg>
-            </button>
-        `
-        this._parent._container.appendChild(this._menu)
-
-        //add click events
-        this._menu.querySelectorAll(`.${this._classButtonHighligh}`)
-            .forEach(e=>{
-                this._addClick = this._addClick.bind(this)
-                e.removeEventListener('click', this._addClick)
-                e.addEventListener('click', this._addClick)
-            })
-
-        this._menu.querySelectorAll(`#${this._idButtonNote}`)
-            .forEach(e=>{
-                this._addNoteClick = this._addNoteClick.bind(this)
-                e.removeEventListener('click', this._addNoteClick)
-                e.addEventListener('click', this._addNoteClick)
-            })
-    }
-
-    /* Styles */
-    _initStyles() {
-        if (this._parent._container.querySelector(`#${this._idCss}`))
-            return
-
-        const style = this._parent._document.createElement('style')
-        style.id = this._idCss
-        style.innerHTML = `
-            #${this._idMenu} {
-                position: absolute !important;
-                display: flex !important;
-                
-                z-index: 99999999 !important;
-                background-color: transparent !important;
-                margin: 4px !important;
-                width: auto !important;
-                height: auto !important;
-                left: 0 !important; top: 0 !important;
-                box-shadow: none !important;
-                animation: none !important;
-                transition: opacity .15s ease-in-out !important;
-                border: 0 !important;
-                padding: 0 !important;
-                backdrop-filter: blur(20px) !important;
-                -webkit-backdrop-filter: blur(20px) !important;
-            }
-            #${this._idMenu}, #${this._idMenu} *, #${this._idMenu}:after {
-                border-radius: 5px !important;
-            }
-            #${this._idMenu}:after {
-                content: '' !important;
-                position: absolute !important;
-                left: 0 !important; top: 0 !important; right: 0 !important; bottom: 0 !important;
-                z-index: -1 !important;
-                background-color: Menu !important;
-                box-shadow: 0 0 0 0.5px GrayText, 0 5px 30px rgb(0 0 0 / 30%) !important;
-            }
-            @supports (backdrop-filter: blur(20px)) {
-                #${this._idMenu}:after { opacity: .6 !important; }
-            }
-            @supports (-webkit-backdrop-filter: blur(20px)) {
-                #${this._idMenu}:after {
-                    opacity: .6 !important;
-                    background-color: Window !important;
-                }
-            }
-            #${this._idMenu}, #${this._idMenu} * {
-                box-sizing: border-box !important;
-                user-select: none !important;
-                -webkit-user-select: none !important;
-            }
-            #${this._idMenu}[hidden] {
-                pointer-events: none !important;
-                opacity: 0 !important;
-            }
-
-            /* Dropdown */
-            #${this._idMenu} > li {
-                display: block !important;
-                max-height: 32px !important;
-                transition: max-height .15s ease-in !important;
-                transition-delay: .25s !important;
-                overflow: hidden !important;
-            }
-
-            #${this._idMenu} > li:hover:not(:active) {
-                max-height: ${this._colors.length * 32}px !important;
-            }
-
-            /* Buttons */
-            #${this._idMenu} button {
-                cursor: pointer !important;
-                color: WindowText !important;
-                width: 32px !important;
-                height: 32px !important;
-                appearance: none !important;
-                background: transparent !important;
-                border: 0 !important;
-                box-shadow: none !important;
-                margin: 0 !important;
-                padding: 0 !important;
-                display: flex !important;
-                align-items: center !important;
-                justify-content: center !important;
-                transition: background .1s linear, color .1s linear !important;
-                filter: none !important;
-            }
-            #${this._idMenu} button:hover {
-                background: rgba(100,100,100,.3) !important;
-            }
-            #${this._idMenu} button:active {
-                filter: brightness(50%) !important;
-            }
-
-            #${this._idMenu} * {
-                fill: currentColor !important;
-            }
-
-            /* Color */
-            #${this._idMenu} button[${this._attrColor}]:before {
-                content: '' !important;
-                display: block !important;
-                width: 18px !important;
-                height: 18px !important;
-                border-radius: 18px !important;
-                box-shadow: inset 0 0 0 0.5px rgba(0,0,0,.4) !important;
-                background-image: linear-gradient(to bottom, rgba(255,255,255,.5) 0, rgba(255,255,255,.4) 100%) !important;
-            }
-            ${this._colors.map(color=>`
-                #${this._idMenu} button[${this._attrColor}=${color}]:before { background-color: ${color} !important; }
-            `).join('')}
-        `
-        this._parent._container.appendChild(style)
+        this._selectTimeout = setTimeout(this.render, this._haveSelection() ? 200 : 0)
     }
 }
 
@@ -326,7 +351,7 @@ class RdHighlight {
         this._window = this._document.defaultView
 
         //init select menu
-        this._select = new RdSelect(this)
+        this._select = new RdSelection(this)
 
         //bind
         this._markClick = this._markClick.bind(this)
