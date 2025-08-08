1ae4a9fefa06bb451af85d187382168b410cf42a
Rustem Mussabekov <winlinx@ya.ru>
2025-08-08 16:55:49 +0300

    Fix build when 'dist' folder is not exists

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve, join } from 'path'
+import { resolve, join, dirname } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import fs from 'fs'
@@ -31,8 +31,10 @@ function rnCopyPlugin() {
             for (const fileName in bundle) {
                 const file = bundle[fileName]
                 if (file.type != 'chunk' || !fileName.endsWith('.js')) continue
+                const path = join(options.dir, fileName.replace('.js', '.string.js'))
+                fs.mkdirSync(dirname(path), { recursive: true })
                 fs.writeFileSync(
-                    join(options.dir, fileName.replace('.js', '.string.js')),
+                    path,
                     `export default ${JSON.stringify(file.code)}`,
                     'utf8'
                 )
